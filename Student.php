7d19d0fd1c642920ccdbfd9ba823e45f395754df
wid <wid@windows10.microdone.cn>
2020-09-22 11:03:58 +0800

    I add a constructor that initialize the properties and add a couple of mutator methods.

---
+++ Student.php
@@ -0,0 +1,14 @@
+function __construct() {
+$this->surname = '';
+$this->first_name = '';
+$this->emails = array();
+$this->grades = array();
+}
+
+
+function add_email($which,$address) {
+$this->emails[$which] = $address;
+}
+function add_grade($grade) {
+$this->grades[] = $grade;
+}
